7163bf95972278c2b902f016c93ff533c3ebbb24
Marco Antônio R. A. B. Ribeiro <marcoantoniorabr@gmail.com>
2026-03-10 20:41:15 -0300

    "comentarios adicionados ao projeto da estrura VetorInt"

--- aula2/src/main.c
+++ aula2/src/main.c
@@ -2,21 +2,24 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-struct VetorInt
+typedef struct VetorInt
 {
     /* data */
     int *lista;
     size_t comprimento;
-};
+} VetorInt;
 
-struct VetorInt *constructorVetorInt(size_t comprimento) {
+// Função construtura do tipo vetor inteiro.
+// ...
+// Recebe o tamanho que o vetor que será contruido e retorna o ponteiro aonde ele esta localizado ou retorna NULL em caso de falha.
+VetorInt *constructorVetorInt(size_t comprimento) {
     if(comprimento == 0)
     {
         printf_s("Erro: o comprimento não pode ser 0.\n");
         return NULL;
     }
 
-    struct VetorInt *pointerVetor = (struct VetorInt *) calloc(1, sizeof(*pointerVetor));
+    VetorInt *pointerVetor = (struct VetorInt *) calloc(1, sizeof(*pointerVetor));
     if(pointerVetor == NULL) {
         printf_s("Erro: falha ao alocar memoria de vetor.\n");
         free(pointerVetor);
@@ -34,6 +37,9 @@ struct VetorInt *constructorVetorInt(size_t comprimento) {
     return pointerVetor;
 }
 
+// Função destrutura do tipo vetor.
+// ...
+// Recebe o ponteiro do vetor apaga todos os valores para 0 e libera seu espaço ocupado.
 void destructorVetorInt(struct VetorInt *vetor) {
     for(int i = 0; i < vetor->comprimento; i++) {
         vetor->lista[i] = 0;
@@ -43,30 +49,49 @@ void destructorVetorInt(struct VetorInt *vetor) {
     free(vetor);
 }
 
+// Função para alterar valor de um item do vetor inteiro.
+// ...
+// saida com codigo -10 = index fora dos limites do tamanho do vetor.
+// ...
+// Recebe o ponteiro do vetor de inteiros a ser alterado o index a posição que será alterada e o valor inteiro que sera inserido no espaço. encerra o programa com um codigo em caso de erro *olhar possiveis codigos.
 void setItemVetorInt(struct VetorInt *vetor, size_t index, int valor) {
     if(index >= vetor->comprimento) {
         printf_s("Erro: index fora dos limites do vetor.\n");
-        return;
+        exit(-10);
     }
     vetor->lista[index] = valor;
 }
 
+// Função para retornar o valor de um item do vetor inteiro.
+// ...
+// saida com codigo -10 = index fora dos limites do tamanho do vetor.
+// ...
+// Recebe o ponteiro do vetor de inteiros que se deseja retornar o valor e a posição do valor a ser retornada. encerra o programa com um codigo em caso de erro *olhar possiveis codigos.
 int getItemVetorInt(struct VetorInt *vetor, size_t index) {
     if(index >= vetor->comprimento) {
         printf_s("Erro: index fora dos limites do vetor.\n");
-        return 0;
+        exit(-10);
     }
     return vetor->lista[index];
 }
 
+// Função para retornar o tamanho do vetor.
+// ...
+// Recebe o ponteiro do vetor e retorna o seu tamanho.
 size_t getComprimentoVetorInt(struct VetorInt *vetor) {
     return vetor->comprimento;
 }
 
+// Função que apaga um item do vetor e retorna seu valor.
+// ...
+// saida com codigo -10 = index fora dos limites do tamanho do vetor.
+// saida com codigo -11 = falha ao alocar memoria do vetor.
+// ...
+// Recebe o ponteiro do vetor e o indexador da posição desejada. encerra o programa com um codigo em caso de erro *olhar possiveis codigos.
 int popItemVetorInt(struct VetorInt *vetor, size_t index) {
     if(index >= vetor->comprimento) {
         printf_s("Erro: index fora dos limites do vetor.\n");
-        return 0;
+        exit(-10);
     }
     int popped = vetor->lista[index];
     for(int i = index; i < vetor->comprimento - 1; i++) {
@@ -77,26 +102,34 @@ int popItemVetorInt(struct VetorInt *vetor, size_t index) {
     if(ponteiroTemporario == NULL) {
         free(ponteiroTemporario);
         printf_s("Erro: falha ao alocar memoria de vetor.\n");
-        return 0;
+        exit(-11);
     }
     vetor->lista = ponteiroTemporario;
     vetor->comprimento = novoTamanho;
     return popped;
 }
 
+// Função que adiciona mais um item ao vetor na sua ultima posição.
+// ...
+// saida com codigo -11 = falha ao alocar memoria do vetor.
+// ...
+// Recebe o ponteiro do vetor e o valor do novo item que sera inserido. encerra o programa com um codigo em caso de erro *olhar possiveis codigos.
 void appendItemVetorInt(struct VetorInt *vetor, int valor) {
     size_t novoTamanho = vetor->comprimento + 1;
     int *ponteiroTemporario = (int *) realloc(vetor->lista, (novoTamanho * sizeof(int)));
     if(ponteiroTemporario == NULL) {
         free(ponteiroTemporario);
         printf_s("Erro: falha ao alocar memoria de vetor.\n");
-        return;
+        exit(-11);
     }
     vetor->lista = ponteiroTemporario;
     vetor->comprimento = novoTamanho;
     vetor->lista[novoTamanho - 1] = valor;
 }
 
+// Função que imprime todos os items de um vetor entre "[]" e separados por ", ".
+// ...
+// Recebe o ponteiro do vetor e imprime os valores formatados.
 void printVetorint(struct VetorInt *vetor) {
     printf_s("[");
     for(int i = 0; i < vetor->comprimento; i++) {
@@ -107,14 +140,17 @@ void printVetorint(struct VetorInt *vetor) {
     }
 }
 
-struct VetorInt *criarVetorIntDeArray(int *array, size_t comprimento) {
+// Função construtura do tipo vetor inteiro já populada com dados apartir de uma array já existente.
+// ...
+// Recebe o ponteiro da array já existente o tamanho da mesma array e retorna o ponteiro aonde ele esta localizado ou retorna NULL em caso de falha.
+VetorInt *criarVetorIntDeArray(int *array, size_t comprimento) {
     if(comprimento == 0)
     {
         printf_s("Erro: o comprimento não pode ser 0.\n");
         return NULL;
     }
 
-    struct VetorInt *pointerVetor = (struct VetorInt *) calloc(1, sizeof(*pointerVetor));
+    VetorInt *pointerVetor = (struct VetorInt *) calloc(1, sizeof(*pointerVetor));
     if(pointerVetor == NULL) {
         printf_s("Erro: falha ao alocar memoria de vetor.\n");
         free(pointerVetor);
@@ -136,11 +172,13 @@ struct VetorInt *criarVetorIntDeArray(int *array, size_t comprimento) {
     return pointerVetor;
 }
 
+
+
 int main(void) {
     int array[] = {
         2, 4, 5, 6, 7, 5, 6
     };
-    struct VetorInt *minha_lista = criarVetorIntDeArray(array, (sizeof(array) / sizeof(array[0])));
+    VetorInt *minha_lista = criarVetorIntDeArray(array, (sizeof(array) / sizeof(array[0])));
 
     printf_s("%zd\n", getComprimentoVetorInt(minha_lista));
     printVetorint(minha_lista);
